211ec3479181c34655147680f1fc4065dc99c98a
yinguilong <yinguilong@hotmail.com>
2019-06-24 21:49:33 +0800

    增加redis哨兵模式demo

---
+++ redis-sentinel-demo/src/main/java/com/yin/redissentineldemo/HomeController.java
@@ -0,0 +1,30 @@
+package com.yin.redissentineldemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * 类的描述
+ *
+ * @author yinguilong
+ * @Time 2019/6/23 15:15
+ **/
+@RestController
+public class HomeController {
+    @Autowired
+    RedisTemplate<String, Object> redisTemplate;
+    @RequestMapping("/")
+    public  String index(@RequestParam String a)
+    {
+        try {
+            redisTemplate.opsForValue().set("yin",a);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        return  a;
+    }
+}
